90323ecd7b756931d5e0c99e1da011ffbb22eb9b
ibraheemkabir <ibraheemkabir96@gmail.com>
2020-01-05 23:01:37 +0100

    initiate request

--- App.js
+++ App.js
@@ -1,42 +1,228 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 // import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+import RNWalletConnect from "@walletconnect/react-native";
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [barCodeResult, setBarCodeResult] = useState('');
-  const [type, setType] = useState(Camera.Constants.Type.back);
+class App extends React.Component{
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasPermission: null,
+      barCodeResult: '',
+      showCamera: false,
+      type: Camera.Constants.Type.back,
+      walletConnector: null 
+    }  
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+ 
+
+  async componentDidMount(){
+    const { status } = await Camera.requestPermissionsAsync();
+    if(status === 'granted'){
+      this.setState({hasPermission: 'granted'})
+    }
   }
 
-  function onBarCodeScanned(dataObject){
-    return setBarCodeResult(dataObject.data);
+  initWalletConnect = async (uri) => {
+
+    try {
+      const walletConnector = new RNWalletConnect(
+        {uri},{
+          clientMeta: {
+            // Required
+            description: "WalletConnect Developer App",
+            url: "https://walletconnect.org",
+            icons: ["https://walletconnect.org/walletconnect-logo.png"],
+            name: "WalletConnect",
+            ssl: true
+          },
+          // push: {
+          //   // Optional
+          //   url: "https://push.walletconnect.org",
+          //   type: "fcm",
+          //   token: token,
+          //   peerMeta: true,
+          //   language: language
+          // }
+        }
+      );
+
+
+
+
+      if (!walletConnector.connected) {
+        await walletConnector.createSession()
+      }
+
+      this.subscribeToEvents();
+
+      await this.setState({
+        loading: false,
+        walletConnector,
+        uri: walletConnector.uri
+      });      
+
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  
+  approveSession = () => {
+    const { walletConnector, chainId, address } = this.state;
+    if (walletConnector) {
+      walletConnector.approveSession({ chainId, accounts: [address] });
+    }
+    this.setState({ walletConnector });
+  };
+
+  rejectSession = () => {
+    const { walletConnector } = this.state;
+    if (walletConnector) {
+      walletConnector.rejectSession();
+    }
+    this.setState({ walletConnector });
+  };
+
+  
+  killSession = () => {
+    const { walletConnector } = this.state;
+    if (walletConnector) {
+      walletConnector.killSession();
+    }
+    this.resetApp();
+  };
+
+  resetApp = async () => {
+    await this.setState({ ...INITIAL_STATE });
+    this.initWallet();
+  };
+
+  subscribeToEvents = () => {
+
+    const {walletConnector} = this.state;
+
+    if(walletConnector){
+
+      walletConnector.on("session_request", (error, payload) => {
+        console.log('walletConnector.on("session_request")'); // tslint:disable-line
+
+        if (error) {
+          throw error;
+        }
+
+        const { peerMeta } = payload.params[0];
+        this.setState({ peerMeta });
+      });
+
+      walletConnector.on("session_update", (error, payload) => {
+        console.log('walletConnector.on("session_update")'); // tslint:disable-line
+
+        if (error) {
+          throw error;
+        }
+      });
+
+      walletConnector.on("call_request", (error, payload) => {
+        console.log('walletConnector.on("call_request")'); // tslint:disable-line
+
+        if (error) {
+          throw error;
+        }
+
+        const requests = [...this.state.requests, payload];
+        this.setState({ requests });
+      });
+
+      walletConnector.on("connect", (error, payload) => {
+        console.log('walletConnector.on("connect")'); // tslint:disable-line
+
+        if (error) {
+          throw error;
+        }
+
+        console.log(this.state,'connected----++')
+
+        this.setState({ connected: true });
+      });
+
+      walletConnector.on("disconnect", (error, payload) => {
+        console.log('walletConnector.on("disconnect")'); // tslint:disable-line
+
+        if (error) {
+          throw error;
+        }
+
+        this.resetApp();
+      });
+
+      if (walletConnector.connected) {
+        console.log(walletConnector.chainId,walletConnector.accounts);
+        const { chainId, accounts } = walletConnector;
+        console.log(chainId,accounts)
+        const address = accounts[0];
+        // updateWallet(address, chainId);
+        this.setState({
+          connected: true,
+          address,
+          chainId
+        });    
+        this.setState({ walletConnector });
+      }
+    }
+   
+   
+  };
+  
+  
+
+  onBarCodeScanned = (dataObject) =>{
+    this.setState({showCamera: false, barCodeResult:dataObject.data})
+    this.initWalletConnect(dataObject.data);
   }
 
-  return (
-    <React.Fragment>
-      <Camera
-        onBarCodeScanned={(dataObject) => onBarCodeScanned(dataObject)}
-        style={styles.cameraView}
-        type={type}
-      >
-      </Camera>
-      <Text>{barCodeResult !== '' ? barCodeResult : ''}</Text>
-    </React.Fragment>
-  );
+
+  render(){
+    const {hasPermission,barCodeResult,showCamera,type} = this.state;
+
+    
+    return (
+      <React.Fragment>
+        { hasPermission === null 
+          ?
+            <View />
+          : hasPermission === false ?
+            <Text>No access to camera</Text>
+          : 
+            <View style={styles.container}>
+            {
+              showCamera ?
+              <>
+                <Camera
+                  onBarCodeScanned={(dataObject) => this.onBarCodeScanned(dataObject)}
+                  style={styles.cameraView}
+                  type={type}
+                />
+                <View style={styles.scanBtn}>
+                  <Button
+                  title="Close"
+                  onPress={ () => this.setState({showCamera: false})}
+                  />
+                </View>
+              </>
+              :<Button
+              title="Scan QrCode"
+              onPress={ () => this.setState({showCamera: true}) }
+            />
+            }
+              <Text>{barCodeResult !== '' ? barCodeResult : ''}</Text>
+          </View>
+        }
+      </React.Fragment>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -51,4 +237,10 @@ const styles = StyleSheet.create({
     height: 250,
     borderRadius: 30,
   },
+  scanBtn: {
+    paddingTop: '5%',
+    width: '50%'
+  }
 });
+
+export default App;
